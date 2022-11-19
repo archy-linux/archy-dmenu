d9813b385a7bf92e8a7623b822f8eb320b6921f9
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-11-19 22:59:12 +0200

    Applay the grid patch yoo

--- src/config.h
+++ src/config.h
@@ -18,8 +18,9 @@ static const char *colors[SchemeLast][2] = {
         [SchemeOut] = {"#000000", "#00ffff"},
         [SchemeOutHighlight] = {"#ffc978", "#00ffff"},
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines = 0;
+static unsigned int columns = 0;
 
 /*
  * Characters not considered part of a word while deleting words
