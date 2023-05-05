2e8106d416900f2fd13b1609e4b9a61b963e7464
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-05-05 17:41:58 +0300

    [update] Show in bottom by default

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 700;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
